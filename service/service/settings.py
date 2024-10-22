1d9b601a645624e054e1611a14bbe14d6b284b5d
thesobirov <shahzodsobirjonov9@gmail.com>
2024-10-22 14:05:44 +0500

    Day 3 - 1

--- service/service/settings.py
+++ service/service/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-
     'clients',
     'services',
 ]
@@ -131,3 +130,18 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler'
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG'
+        }
+    }
+}
--- service/services/views.py
+++ service/services/views.py
@@ -1,3 +1,16 @@
+from django.db.models import Prefetch
 from django.shortcuts import render
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
-# Create your views here.
+from clients.models import Clients
+from services.models import Subscription
+from services.serializers import SubscriptionSerializer
+
+
+class SubscriptionView(ReadOnlyModelViewSet):
+    queryset = Subscription.objects.all().prefetch_related(
+        Prefetch('client',
+                 queryset=Clients.objects.all().select_related('user').only('company_name',
+                                                                            'user__email'))
+    )
+    serializer_class = SubscriptionSerializer
